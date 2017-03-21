51076e2340bd5114a0d5b609a1daba81608f7196
okamuuu <okamuuu@gmail.com>
2017-03-21 12:32:26 +0900

    fix images

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # courrier.jp hackathon
 
-http://courrierjapon024.peatix.com/
+![courrier.jp hackathon example](https://cloud.githubusercontent.com/assets/123702/24131454/1f7c2b92-0e32-11e7-9e4a-2df40ca809ae.gif)
 
 ## USAGE
 
@@ -9,3 +9,7 @@ npm run serve
 npm run start
 open http://localhost:3000
 ```
+
+## SEE ALSO
+
+http://courrierjapon024.peatix.com/
--- articles.js
+++ articles.js
@@ -1,5 +1,16 @@
 const faker = require('faker')
 
+const images = [
+  "https://images.unsplash.com/photo-1473172707857-f9e276582ab6?w=300",
+  "https://images.unsplash.com/photo-1484199408980-5918a796a53f?w=300",
+  "https://images.unsplash.com/photo-1462332420958-a05d1e002413?w=300",
+  "https://images.unsplash.com/photo-1423460359171-71432dd68be8?w=300",
+  "https://images.unsplash.com/photo-1435777940218-be0b632d06db?w=300",
+  "https://images.unsplash.com/photo-1476610182048-b716b8518aae?w=300",
+  "https://images.unsplash.com/photo-1477915737647-b5246ee6de6f?w=300",
+  "https://images.unsplash.com/photo-1483197452165-7abc4b248905?w=300"
+]
+
 module.exports = function() {
   const articles = []
 
@@ -9,8 +20,8 @@ module.exports = function() {
       "id": id,
       "post_title": faker.lorem.words(),
       "post_content": faker.lorem.paragraphs(),
-      "image": { src: [faker.image.imageUrl()] },
-      "pined": false
+      "image": { src: [images[id % images.length]] },
+      "pinged": 0
     })
   }
 
